70be9a844f1c5cd2d3639d9934c2aa438c7b4623
Noah Moroze <noahm@moroze.com>
2015-10-13 16:00:57 -0400

    Speed tweaks

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -10,8 +10,9 @@ import com.qualcomm.robotcore.util.Range;
  */
 public class TeleOp extends OpMode {
     final static double NOODLER_POWER = 1.0;
-    final static double DUMP_MAX_RANGE = 0.7;
-    final static double DUMP_MIN_RANGE = 0.3;
+    final static double NOODLER_REVERSE_POWER = -1.0;
+    final static double DUMP_MAX_RANGE = 0.6;
+    final static double DUMP_MIN_RANGE = 0.4;
     final static double LIFT_MAX_RANGE = 1.0;
     final static double LIFT_MIN_RANGE = 0.0;
 
@@ -21,6 +22,8 @@ public class TeleOp extends OpMode {
     DcMotor noodler;
     Servo dumpServo1, dumpServo2, liftServo;
 
+    boolean driveInverted = false;
+    boolean bWasDown = false;
     double dumpServoPosition = 0.5;
     double liftServoPosition = 0.3;
 
@@ -42,13 +45,32 @@ public class TeleOp extends OpMode {
         gamepad1.setJoystickDeadzone(0.1f);
         gamepad2.setJoystickDeadzone(0.1f);
 
-        leftDrivePair.setPower(-gamepad1.left_stick_y);
-        rightDrivePair.setPower(-gamepad1.right_stick_y);
+        double leftPower = scaleInput(-gamepad1.left_stick_y);
+        double rightPower = scaleInput(-gamepad1.right_stick_y);
 
-        if(gamepad1.right_bumper)
-            noodler.setPower(NOODLER_POWER);
+        if(driveInverted) {
+            double temp = -leftPower;
+            leftPower = -rightPower;
+            rightPower = temp;
+        }
+
+        leftDrivePair.setPower(leftPower);
+        rightDrivePair.setPower(rightPower);
+
+        if(gamepad1.right_trigger > 0.1)
+            noodler.setPower(scaleInput(gamepad1.right_trigger));
+        else if(gamepad1.right_bumper)
+            noodler.setPower(NOODLER_REVERSE_POWER);
         else
             noodler.setPower(0);
+
+        if(gamepad1.b && !bWasDown) {
+            driveInverted = !driveInverted;
+            bWasDown = true;
+        }
+        else if(!gamepad1.b) {
+            bWasDown = false;
+        }
 
         if(gamepad2.dpad_up)
             liftServoPosition += LIFT_DELTA;
@@ -59,6 +81,8 @@ public class TeleOp extends OpMode {
             dumpServoPosition = DUMP_MAX_RANGE;
         else if(gamepad2.y)
             dumpServoPosition = DUMP_MIN_RANGE;
+        else if(liftServoPosition > 0.4)
+            dumpServoPosition = 0.48;
         else
             dumpServoPosition = 0.5;
 
@@ -67,6 +91,7 @@ public class TeleOp extends OpMode {
         liftServo.setPosition(liftServoPosition);
         setDumpServos(dumpServoPosition);
 
+        telemetry.addData("liftServoPosition", liftServoPosition);
         try {
             Thread.sleep(10);
         } catch (InterruptedException e) {
@@ -77,5 +102,39 @@ public class TeleOp extends OpMode {
     public void setDumpServos(double pos) {
         dumpServo1.setPosition(pos);
         dumpServo2.setPosition(1.0 - pos);
+    }
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 }
