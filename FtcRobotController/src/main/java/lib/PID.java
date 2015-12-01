7174639b2cb0e5dbdf104642cea500c6c244d9ba
Noah Moroze <noahm@moroze.com>
2015-11-30 23:25:20 -0500

    First commit of PID lib

---
+++ FtcRobotController/src/main/java/lib/PID.java
@@ -0,0 +1,98 @@
+package lib;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by noah on 11/30/15.
+ */
+public class PID {
+    private final double Kp;
+    private final double Ki;
+    private final double Kd;
+    private final boolean reversed;
+    private final double iCap;
+    private final double targetThresh;
+
+    private double target;
+    private double minOutput;
+    private double maxOutput;
+
+    private double iTerm;
+    private double prevError;
+
+    private ElapsedTime timer;
+
+    public PID(double Kp, double Ki, double Kd) {
+        this(Kp, Ki, Kd, false, 0, -1);
+    }
+
+    public PID(double Kp, double Ki, double Kd, boolean reversed, double targetThresh) {
+        this(Kp, Ki, Kd, reversed, targetThresh, -1);
+    }
+
+    public PID(double Kp, double Ki, double Kd, boolean reversed, double targetThresh, double iCap) {
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+        this.reversed = reversed;
+        this.targetThresh = targetThresh;
+        this.iCap = iCap;
+        this.target = 0;
+        timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        reset();
+    }
+
+    public double update(double current) {
+        double dt = timer.time(); // get time since last update
+        double error = target - current;
+        if(prevError == -1)
+            prevError = error;
+        iTerm += error * dt;
+        if(iCap != -1)
+            iTerm = Range.clip(iTerm, -1*iCap, iCap);
+
+        double dTerm = (error - prevError)/dt;
+
+        double output = Kp * error + Ki * iTerm - Kd * dTerm;
+        output = Range.clip(output, minOutput, maxOutput);
+
+        if(reversed)
+            output *= -1;
+
+        prevError = error;
+        timer.reset(); // reset timer and start counting time till next update
+        return output;
+    }
+
+    public void reset() {
+        iTerm = 0;
+        prevError = -1;
+        timer.reset();
+    }
+
+    public boolean setMinOutput(double minOutput) {
+        if(minOutput > this.maxOutput)
+            return false;
+        this.minOutput = minOutput;
+        return true;
+    }
+
+    public boolean setMaxOutput(double maxOutput) {
+        if(maxOutput < this.minOutput)
+            return false;
+
+        this.maxOutput = maxOutput;
+        return true;
+    }
+
+    public void setTarget(double target) {
+        this.target = target;
+    }
+
+    public boolean isAtTarget() {
+        if(prevError == -1)
+            return false;
+        return (targetThresh >= Math.abs(prevError));
+    }
+}
