afbe7d5ce924f0b26ba57aece214a32cc1811288
Vivek Bhupatiraju <triforce314@gmail.com>
2015-12-11 11:03:18 -0500

    simplified code

--- FtcRobotController/src/main/java/lib/Filter.java
+++ FtcRobotController/src/main/java/lib/Filter.java
@@ -40,37 +40,29 @@ public class Filter {
         }
         return Math.sqrt((squareSum) / (nums.length - 1));
     }
+    private void addToAverage(double val){
+        data[index] = val;
+        if (index == size - 1) {
+            filled = true;
+        }
+
+        index++;
+        index %= size;
+
+        avg *= size;
+        avg -= data[index];
+        avg += val;
+        avg /= size;
+    }
     public void update(double val){
         if (stdevs >-1) {
 
             if (Math.abs(val - avg) < findDeviation(data) * stdevs) {
-                data[index] = val;
-                if (index == size - 1) {
-                    filled = true;
-                }
-
-                index++;
-                index %= size;
-
-                avg *= size;
-                avg -= data[index];
-                avg += val;
-                avg /= size;
+                addToAverage(val);
             }
         }
         else {
-            data[index] = val;
-            if (index == size - 1) {
-                filled = true;
-            }
-
-            index++;
-            index %= size;
-
-            avg *= size;
-            avg -= data[index];
-            avg += val;
-            avg /= size;
+            addToAverage(val);
         }
     }
 
@@ -81,4 +73,6 @@ public class Filter {
     public double getAvg(){
         return avg;
     }
+    public double getLastValue(){ return data[size-1]; }
+
 }
