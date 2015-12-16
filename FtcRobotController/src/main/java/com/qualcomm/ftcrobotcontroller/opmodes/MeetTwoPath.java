2cca20c60ced5122638a6d82fceb78e02c1d6932
Luke West <l.s.west88@gmail.com>
2015-12-16 14:38:53 -0500

    Stripping more from MeetTwoPath.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MeetTwoPath.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MeetTwoPath.java
@@ -17,33 +17,16 @@ public class MeetTwoPath extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         Robot dave = new Robot(hardwareMap, telemetry, this); // makes Robot "dave"
-        String x = "NOT CALIBRATED";
-
-        Robot.state = new SensorState(hardwareMap, 1, 0);
-        Robot.state.registerSensor("hero", SensorState.SensorType.GYRO, true, 12);
-        Robot.state.registerSensor("mr", SensorState.SensorType.COLOR, false, 12);
-        Robot.state.registerSensor("mrs", SensorState.SensorType.LIGHT, true, 12);
-        Robot.state.registerSensor("rearUltra", SensorState.SensorType.ULTRASONIC, true, 50);
-        Robot.state.registerSensor("frontUltra", SensorState.SensorType.ULTRASONIC, true, 50);
-        Thread state_thread = new Thread(Robot.state);
-        state_thread.start();
-
-        waitForStart();
-        while (Robot.state.calibrating("hero")) {
-            x = "calibrated";
-            Robot.tel.addData("Reading", "");
-        }
 
         TwoWheelDrive dave_train = new TwoWheelDrive(hardwareMap.dcMotor.get("leftdrive"), true,
                 hardwareMap.dcMotor.get("rightdrive"), false, 4);
 
         dave.registerDriveTrain(dave_train);
-        dave.registerUltrasonicServo("frontUltra", "frontSwivel");
+
+        waitForStart();
 
         //movement//
 
-        dave_train.moveDistanceWithCorrections(.5, 60);
-
-        state_thread.interrupt();
+        dave_train.moveDistance(0.5, 60);
     }
 }
