78f41a845fa8e314ec1624685bdce4697f8568e1
Luke West <l.s.west88@gmail.com>
2016-01-07 15:37:47 -0500

    Removed all the repeated code from TwoWheelDrive and FourWheelDrive and moved it to Drivetrain, easier to maintain and use

--- FtcRobotController/src/main/java/lib/DriveTrain.java
+++ FtcRobotController/src/main/java/lib/DriveTrain.java
@@ -1,26 +1,181 @@
 package lib;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by luke on 10/7/15.
  */
-public interface DriveTrain {
-    // DriveTrain describes the drive setup of the robot.
-    // This can be four wheel drive, two wheel drive, treads, etc, so we need to implement
-    // this interface in other classes.
-    public void moveDistance(double power, double d);
-    public void turnWithEncoders(double power, double angle);
-    public void move(double power);
-    public void move(double power, String gyro_name, LinearOpMode waiter);
-    public void stopMove();
-    public void setLeftMotors(double power);
-    public void setRightMotors(double power);
-    public void resetEncoders();
-    public int getEncoders();
-    public double getActualHeading(String name);
-    public double getExpectedHeading();
+public class DriveTrain {
+    // DriveTrain is the base class for TwoWheelDrive and FourWheelDrive, which only differ on the most
+    // basic levels of wheel control.
 
+    //OVERRIDDEN:
+    public void setLeftMotors(double power) {}
+    public void setRightMotors(double power) {}
 
-//    public void moveWithCorrection(double power);
+    public void resetEncoders() {}
+    public int getEncoders() {return 0;}
+    // END OF OVERRIDDEN
+
+    protected double wheel_circumference;
+    protected int rightEncoder, leftEncoder;
+
+    protected Thread move_thread;
+    protected MovementThread mover;
+    protected boolean thread_running;
+
+    protected double expectedHeading;
+    protected final double TURN_SCALAR = 0.23;
+
+    public double getActualHeading(String name) {
+        return Robot.state.getSensorReading(name);
+    }
+
+    public double getExpectedHeading() { return expectedHeading; }
+
+    public double angleDist(double deg1, double deg2) {
+        double absDist = (360 + deg2 - deg1) % 360;
+        if (absDist > 180)
+            absDist -= 360;
+        return absDist;
+    }
+
+    public void moveDistance(double power, double distance){
+        distance = (distance / wheel_circumference) * 1120;
+
+        while (Math.abs(getEncoders()) < distance) {
+            this.setLeftMotors(power);
+            this.setRightMotors(power);
+        }
+
+        setLeftMotors(0);
+        setRightMotors(0);
+    }
+
+    public void move(double power){
+        setLeftMotors(power);
+        setRightMotors(power);
+    }
+
+    public void moveDistanceWithCorrections(double power, String gyro_name, double distance, LinearOpMode waiter){
+        // 1120 ticks in the encoder
+        resetEncoders();
+        distance = (distance / wheel_circumference) * 1120;
+
+        move(power, gyro_name, waiter);
+        while (Math.abs(getEncoders()) < distance && waiter.opModeIsActive()) {}
+        stopMove();
+
+        setLeftMotors(0);
+        setRightMotors(0);
+    }
+
+    public void move(double power, String gyro_name, LinearOpMode waiter){
+        if(!thread_running) {
+
+            mover = new MovementThread(this, gyro_name, 0, waiter, power);
+            move_thread = new Thread(mover);
+            move_thread.start();
+            thread_running = true;
+        }
+
+        else {
+            mover.setPower(power);
+        }
+    }
+
+    public void stopMove() {
+        if (thread_running){
+            move_thread.interrupt();
+            thread_running = false;
+        }
+    }
+
+    public void turnWithEncoders(double power, double angle) {
+        resetEncoders();
+        double target = TURN_SCALAR * (angle/wheel_circumference) * 1120;
+
+        while(Math.abs(getEncoders()) < target) {
+            setLeftMotors(-power);
+            setRightMotors(power);
+        }
+
+        setLeftMotors(0);
+        setRightMotors(0);
+    }
+
+    public void turnWithGyro(int degrees, String name) {
+        // Stay positive
+        expectedHeading += degrees + 360;
+        expectedHeading %= 360;
+
+        /*
+        In this system, the turnPID's target is the final rotation angle.
+        It sends rotational speed targets to the speedPID, which outputs motor values to add to the
+        existing motor values to reach those speeds.
+         */
+
+        // Tuned values
+        PID turnPID = new PID(1.6, 0, 0, true, 1.1);
+        PID speedPID = new PID(0.00071, 0, 0.0002, false, 0);
+
+        degrees= ((360 - (int) expectedHeading) + 360) % 360;
+        turnPID.setTarget(degrees);
+
+        turnPID.setMaxOutput(75);
+        turnPID.setMinOutput(-75);
+        speedPID.setMaxOutput(0.1);
+        speedPID.setMinOutput(-0.1);
+
+        double reading = Robot.state.getSensorReading(name);
+        double prevReading;
+        double power = 0.9;
+        double time;
+        double angle;
+        double currentSpeed;
+        double angVel;
+
+        Filter filter = new Filter(20);
+
+        prevReading = reading;
+        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+
+        while(Robot.waiter.opModeIsActive() && !turnPID.isAtTarget(10)) {
+            time = timer.time();
+            angle = Robot.state.getSensorReading(name);
+            currentSpeed = angleDist(angle , prevReading)/time;
+            filter.update(currentSpeed);
+
+            angVel = turnPID.updateWithError(angleDist(angle,degrees));
+
+            // Don't want it to be able to slow down too much. Lower cap at 3, + or -
+            if (Math.abs(angVel) < 3) {
+                speedPID.setTarget(Math.signum(angVel)*3);
+            } else{
+                speedPID.setTarget(angVel);
+            }
+
+            power += speedPID.update(filter.getAvg());
+            power = Range.clip(power, -1, 1);
+
+            setLeftMotors(power);
+            setRightMotors(-power);
+
+            prevReading = angle;
+            timer.reset();
+
+            try {
+                Thread.sleep(25);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        setLeftMotors(0);
+        setRightMotors(0);
+    }
 }
--- FtcRobotController/src/main/java/lib/TwoWheelDrive.java
+++ FtcRobotController/src/main/java/lib/TwoWheelDrive.java
@@ -1,41 +1,22 @@
 package lib;
 
-import android.util.Log;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.GyroSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import static java.lang.Thread.*;
 
 /**
  * Created by luke on 10/7/15.
  */
-public class TwoWheelDrive implements DriveTrain {
+public class TwoWheelDrive extends DriveTrain {
     // Diameter and moveDistance should be measured in inches.
 
     private DcMotor leftMotor, rightMotor;
-    double wheel_circumference;
-    private int rightEncoder, leftEncoder;
-    private Thread move_thread;
-    private MovementThread mover;
-    private double expectedHeading;
-    private boolean thread_running;
 
-    private final double TURN_SCALAR = 0.23;
-
-    // Using SensorState, we would not need to keep a reference to Robot
     public TwoWheelDrive (DcMotor leftMotor, boolean leftRev, DcMotor rightMotor, boolean rightRev, double wheel_diameter) {
         this.wheel_circumference = wheel_diameter * Math.PI;
         this.thread_running = false;
-//        this.robot = robot;
-
-
         this.leftMotor = leftMotor;
         this.rightMotor = rightMotor;
-        expectedHeading = 0;
+        this.expectedHeading = 0;
+
         if (leftRev) leftMotor.setDirection(DcMotor.Direction.REVERSE);
         if (rightRev) rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -53,248 +34,13 @@ public class TwoWheelDrive implements DriveTrain {
     }
 
     @Override
-    public void move(double power) {
-        this.setRightMotors(power);
-        this.setLeftMotors(power);
-    }
-
-    public void move(double power, String gyro_name, LinearOpMode waiter) {
-
-        if(!thread_running) {
-
-            mover = new MovementThread(this, gyro_name, 0, waiter, power);
-            move_thread = new Thread(mover);
-            move_thread.start();
-            thread_running = true;
-        }
-
-
-        else {
-            mover.setPower(power);
-        }
-    }
-
-    public void stopMove(){
-        if (thread_running){
-            move_thread.interrupt();
-            thread_running = false;
-        }
-    }
-
     public void resetEncoders() {
         rightEncoder = rightMotor.getCurrentPosition();
         leftEncoder = leftMotor.getCurrentPosition();
     }
 
+    @Override
     public int getEncoders() {
-
-      return (Math.abs(rightMotor.getCurrentPosition() - rightEncoder));
-    }
-
-    @Override
-    public void moveDistance(double power, double d) {
-        // 1120 ticks in the encoder
-        resetEncoders();
-        double distance = (d/wheel_circumference) * 1120;
-
-        while (Math.abs(getEncoders()) < distance) {
-            this.setLeftMotors(power);
-            this.setRightMotors(power);
-        }
-
-        setLeftMotors(0);
-        setRightMotors(0);
-    }
-
-    public void moveDistanceWithCorrections(double power, double d) {
-        // 1120 ticks in the encoder
-        resetEncoders();
-        double distance = (d/wheel_circumference) * 1120;
-        move(power,"hero",Robot.waiter);
-        while (Math.abs(getEncoders()) < distance && Robot.waiter.opModeIsActive()) {
-
-        }
-        stopMove();
-
-        setLeftMotors(0);
-        setRightMotors(0);
-    }
-
-    @Override
-    public void turnWithEncoders(double power, double angle) {
-        resetEncoders();
-        double target = TURN_SCALAR * (angle/wheel_circumference) * 1120;
-
-        while(Math.abs(getEncoders()) < target) {
-            setLeftMotors(-power);
-            setRightMotors(power);
-        }
-
-        setLeftMotors(0);
-        setRightMotors(0);
-    }
-
-    public int distToZero(int angle1){
-        angle1=Math.abs(angle1)%360;
-        if(angle1>180){
-            return 360 - angle1;
-        }
-        else{
-            return angle1;
-        }
-    }
-
-    public void turnWithGyro(int degrees, String name) {
-        expectedHeading += degrees + 360;
-        expectedHeading %= 360;
-
-        // near perfect
-        PID turnPID = new PID(1.6, 0, 0, true, 1.1);
-//        PID turnPID = new PID(2, 0.15, 0.35, true, 1, 0.75, 5);
-        PID speedPID = new PID(0.00071, 0, 0.0002, false, 0);
-        degrees= ((360 - (int)expectedHeading) + 360) % 360;
-
-        turnPID.setTarget(degrees);
-        turnPID.setMaxOutput(75);
-        turnPID.setMinOutput(-75);
-        speedPID.setMaxOutput(0.1);
-        speedPID.setMinOutput(-0.1);
-        double prevReading;
-        double reading = Robot.state.getSensorReading(name);
-
-        double power = 0.9;
-        double time;
-        double angle;
-        double currentSpeed;
-        double angVel;
-        Filter filter = new Filter(20);
-
-        prevReading = reading;
-        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
-
-        while(Robot.waiter.opModeIsActive() && !turnPID.isAtTarget(10)) {
-            time = timer.time();
-
-            angle = Robot.state.getSensorReading(name);
-            currentSpeed = angleDist(angle , prevReading)/time;
-            filter.update(currentSpeed);
-
-            angVel = turnPID.updateWithError(angleDist(angle,degrees));
-            if(Math.abs(angVel) < 3) {
-                speedPID.setTarget(Math.signum(angVel)*3);
-            }
-            else{
-                speedPID.setTarget(angVel);
-            }
-            power += speedPID.update(filter.getAvg());
-            power = Range.clip(power, -1, 1);
-
-            setLeftMotors(power);
-            setRightMotors(-power);
-
-            Robot.tel.addData("angle", Robot.state.getSensorReading(name));
-            Robot.tel.addData("speed",filter.getAvg());
-            Robot.tel.addData("pid", speedPID.update(filter.getAvg()));
-            Robot.tel.addData("power", power);
-
-            prevReading = angle;
-            timer.reset();
-
-            try {
-                Thread.sleep(25);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        setLeftMotors(0);
-        setRightMotors(0);
-
-    }
-
-    public double angleDist(double deg1, double deg2)
-    {
-        double absDist = (360 + deg2 - deg1) % 360;
-        if (absDist > 180)
-            absDist -= 360;
-        return absDist;
-    }
-
-    public void turnWithGyro(int degrees, String name, GyroSensor jiro) {
-        expectedHeading += degrees + 360;
-        expectedHeading %= 360;
-
-        // near perfect
-        PID turnPID = new PID(1.6, 0, 0, true, 1.1);
-//        PID turnPID = new PID(2, 0.15, 0.35, true, 1, 0.75, 5);
-        PID speedPID = new PID(0.00071, 0, 0.0002, false, 0);
-        degrees= ((360 - (int)expectedHeading) + 360) % 360;
-
-        turnPID.setTarget(degrees);
-        turnPID.setMaxOutput(75);
-        turnPID.setMinOutput(-75);
-        speedPID.setMaxOutput(0.1);
-        speedPID.setMinOutput(-0.1);
-        double prevReading;
-        double reading = Robot.state.getSensorReading(name);
-
-        double power = 0.9;
-        double time;
-        double angle;
-        double currentSpeed;
-        double angVel;
-        Filter filter = new Filter(20);
-
-        prevReading = reading;
-        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
-
-        while(Robot.waiter.opModeIsActive() && !turnPID.isAtTarget(10)) {
-            time = timer.time();
-
-            angle = Robot.state.getSensorReading(name);
-            currentSpeed = angleDist(angle , prevReading)/time;
-            filter.update(currentSpeed);
-
-            angVel = turnPID.updateWithError(angleDist(angle,degrees));
-            if(Math.abs(angVel) < 3) {
-                speedPID.setTarget(Math.signum(angVel)*3);
-            }
-            else{
-                speedPID.setTarget(angVel);
-            }
-            power += speedPID.update(filter.getAvg());
-            power = Range.clip(power, -1, 1);
-
-            setLeftMotors(power);
-            setRightMotors(-power);
-
-            Robot.tel.addData("angle", Robot.state.getSensorReading(name));
-            Robot.tel.addData("speed",filter.getAvg());
-            Robot.tel.addData("pid", speedPID.update(filter.getAvg()));
-            Robot.tel.addData("power", power);
-
-            prevReading = angle;
-            timer.reset();
-
-            try {
-                Thread.sleep(25);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        setLeftMotors(0);
-        setRightMotors(0);
-
-    }
-
-    public double getActualHeading(String name) {
-        return Robot.state.getSensorReading(name);
-    }
-
-    public double getExpectedHeading() {
-        return expectedHeading;
+        return (Math.abs(rightMotor.getCurrentPosition() - rightEncoder));
     }
 }
