a626b4dceef719c2ab5292e0e5ba1153acc170a8
Vivek Bhupatiraju <triforce314@gmail.com>
2016-01-21 12:46:54 -0500

    new reverse functions

--- FtcRobotController/src/main/java/simplelib/DriveTrain.java
+++ FtcRobotController/src/main/java/simplelib/DriveTrain.java
@@ -5,50 +5,49 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import java.util.HashMap;
 
 /**
- * Created by Vivek Bhupatiraju on 1/20/16.
+ * Created by Vivek Bhupatiraju (LW helped)
  */
 
 public class DriveTrain
 {
   private final double TURN_SCALAR = 0.23;
-
-  private DcMotor[] left, right;
-  private int[] leftEncoder, rightEncoder;
   private double wheel_circumference, expectedHeading;
 
-  public DriveTrain(DcMotor[] left, boolean[] leftRev,
-                    DcMotor[] right, boolean[] rightRev,
-                    double wheel_diameter)
+  private HashMap<String, DcMotor> left, right;
+  private HashMap<String, int> leftEncoder, rightEncoder;
+
+  public DriveTrain(String[] leftnames, String[] rightnames, double wheel_diameter)
   {
         this.wheel_circumference = Math.PI * wheel_diameter;
 
-        this.left = left;
-        this.right = right;
-
-        for (int i = 0; i < left.length; i++)
-            if (leftRev[i]) this.left[i].setDirection(DcMotor.Direction.REVERSE);
-
-        for (int i = 0; i < right.length; i++)
-            if (rightRev[i]) this.right[i].setDirection(DcMotor.Direction.REVERSE);
+        for (String name : leftnames) left.putIfAbsent(name, SimpleRobot.hmap.dcMotor.get(name));
+        for (String name : rightnames) right.putIfAbsent(name, SimpleRobot.hmap.dcMotor.get(name));
 
         this.leftEncoder = new int[left.length];
         this.rightEncoder = new int[right.length];
         this.resetEncoders();
     }
+    
+    public void reverseMotor(String name) {
+        if (left.containsKey(name)) left[name].setDirection(DcMotor.Direction.REVERSE);
+        else if (right.containsKey(name)) right[name].setDirection(DcMotor.Direction.REVERSE);
+        else throw new RuntimeException("Motor not registered");
+    }
 
     public void setLeftMotors(double power) {
-        for (DcMotor l : left) l.setPower(power);
+        for (DcMotor l : left.values()) l.setPower(power);
     }
 
     public void setRightMotors(double power) {
-        for (DcMotor r : right) r.setPower(power);
+        for (DcMotor r : right.values()) r.setPower(power);
     }
 
     public void resetEncoders() {
-        for (int i = 0; i < left.length; i++) leftEncoder[i] = left[i].getCurrentPosition();
-        for (int i = 0; i < right.length; i++) rightEncoder[i] = right[i].getCurrentPosition();
+        for (String i : left.keys()) leftEncoder[i] = left[i].getCurrentPosition();
+        for (String i : right.keys()) rightEncoder[i] = right[i].getCurrentPosition();
     }
     
     public int getEncoders() {
