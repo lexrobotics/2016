c842aea27330a606554a07c35269811ebb30815b
Vivek Bhupatiraju <triforce314@gmail.com>
2016-01-20 23:25:39 -0500

    simpleRobot is no longer static

--- FtcRobotController/src/main/java/simplelib/SimpleRobot.java
+++ FtcRobotController/src/main/java/simplelib/SimpleRobot.java
@@ -8,38 +8,38 @@ import com.qualcomm.robotcore.robocol.Telemetry;
 import java.util.HashMap;
 
 /**
- * Created by Vivek Bhupatiraju on 1/20/16.
+ * SimpleRobot is a rewrite of the orignal Robot, with more simplicity.
+ * It is not static; chose to make it an object.
+ * 
  */
 
 public class SimpleRobot
 {
     // Objects for Functions
-    public static HardwareMap hmap;
+    public HardwareMap hmap;
     public static Telemetry tel;
-    public static LinearOpMode opm;
-    public static DriveTrain drivetrain;
+    public LinearOpMode opm;
+    public DriveTrain drivetrain;
 
     // HashMap Stores Moving Parts
-    public static HashMap<String, DcMotor> motors;
-    public static HashMap<String, Servo> servos;
+    public HashMap<String, DcMotor> motors;
+    public HashMap<String, Servo> servos;
 
     // Cannot Make Instance of Mah Boi
-    private SimpleRobot() {}
-
-    public static void init (HardwareMap h, Telemetry t, LinearOpMode o) {
-        hmap = h;
-        tel = t;
-        opm = o;
+    public SimpleRobot(HardwareMap h, Telemetry t, LinearOpMode o) {
+        this.hmap = h;
+        this.tel = t;
+        this.opm = o;
     }
 
-    public static void registerServo(String servoName, double initial_position) {
+    public void registerServo(String servoName, double initial_position) {
         if (!servos.keySet().contains(servoName)){
             hmap.servo.get(servoName).setPosition(initial_position);
             servos.put(servoName, hmap.servo.get(servoName));
         }
     }
 
-    public static void registerMotor (String motorName, double initial_power) {
+    public void registerMotor (String motorName, double initial_power) {
         if (!motors.containsKey(motorName)){
             hmap.dcMotor.get(motorName).setPower(initial_power);
             motors.put(motorName, hmap.dcMotor.get(motorName));
